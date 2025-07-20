f42101e8c9aa54a90875ec7a0672dc97e6e365c6
seojin-kim12 <rlatjwls10403@naver.com>
2025-07-20 21:33:01 +0900

    feat: excel spreadJS designer add

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,72 +1,102 @@
 import '../css/Report.css';
-import React, { useRef } from 'react';
-import * as GC from '@mescius/spread-sheets';
-import { SpreadSheets, Worksheet } from '@mescius/spread-sheets-react';
-import '@mescius/spread-sheets-shapes';
-import '@mescius/spread-sheets-charts';
-import '@mescius/spread-sheets-slicers';
-import '@mescius/spread-sheets-pivot-addon';
-import '@mescius/spread-sheets-io';
-import '@mescius/spread-sheets-resources-ko';
+import React, { useState } from 'react';
 import '@mescius/spread-sheets/styles/gc.spread.sheets.excel2016colorful.css';
+import '@mescius/spread-sheets-resources-ko';
+import '@mescius/spread-sheets-designer/styles/gc.spread.sheets.designer.min.css';
+import '@mescius/spread-sheets-designer-resources-ko';
+import { Designer } from '@mescius/spread-sheets-designer-react';
+import * as GC from '@mescius/spread-sheets';
 import ImportFile from '../components/ImportFile';
 import Export_Excel from '../components/Export_Excel';
-import GetCelData from '../components/GetCelData';
 
 GC.Spread.Common.CultureManager.culture('ko-kr');
 
 const Report = () => {
-    const [minutes, setMinutes] = React.useState('export');
-    const [spread, setSpread] = React.useState(null);
+  const [minutes, setMinutes] = useState('export');
+  const [spread, setSpread] = useState(null);
 
-    // 스프레드 시트 사이즈
-    let hostStyle = {
-        width: '100%',
-        height: '380px',
-    };
+  const onDesignerInitialized = (designer) => {
+    const workbook = designer.getWorkbook();
+    setSpread(workbook);
+    workbook.setSheetCount(5);
+    workbook.options.newTabVisible = false;
+  };
 
-    let initSpread = function (spread) {
-        setSpread(spread);
-    };
+  const config = GC.Spread.Sheets.Designer.DefaultConfig;
 
-    return (
+  config.ribbon[0].buttonGroups.unshift({
+    label: '작업',
+    thumbnailClass: 'work',
+    commandGroup: {
+      children: [
+        {
+          direction: 'vertical',
+          commands: ['addRowToLast']
+        },
+        {
+          direction: 'vertical',
+          commands: ['updateToGrid']
+        }
+      ]
+    }
+  });
+
+  config.commandMap = {
+    addRowToLast: {
+      title: '행 추가',
+      text: '행 추가',
+      iconClass: 'add-row-button',
+      bigButton: true,
+      commandName: 'addRowToLast',
+      execute: function (context, propertyName, fontItalicChecked) {
+        const sheet = spread.getActiveSheet();
+        if (sheet && sheet.getDataSource()) {
+          sheet.addRows(sheet.getRowCount(), 1);
+        }
+      }
+    },
+    updateToGrid: {
+      title: '업데이트',
+      text: '업데이트',
+      iconClass: 'update-to-grid-button',
+      bigButton: true,
+      commandName: 'updateToGrid',
+      execute: function () {
+        alert('업데이트 버튼 클릭됨 (예시)');
+      }
+    }
+  };
+
+  return (
+    <div>
+      <div style={{ height: '750px', width: '70%' }}>
+        <Designer
+          styleInfo={{ width: '100%', height: '100%' }}
+          config={config}
+          designerInitialized={onDesignerInitialized}
+        />
+      </div>
+
+      <div className="bottom">
         <div>
-        <div className="row">
-          <div className="left">
-            <SpreadSheets
-              workbookInitialized={(spread) => initSpread(spread)}
-              hostStyle={hostStyle}
-            >
-              <Worksheet></Worksheet>
-            </SpreadSheets>
-          </div>
-  
-          <div className="right">
-            <div>
-              <h4> Excel 가져오기 </h4>
-              <input type="file" name="files[]" id="fileDemo" accept=".xlsx" />
-              <button onClick={() => ImportFile(spread)}>불러오기</button>
-            </div>
-            <div>
-              <h4> Excel 내보내기 </h4>
-              파일명:
-              <input
-                type="text"
-                id="exportFileName"
-                placeholder="Export file name"
-                value={minutes}
-                onChange={(e) => setMinutes(e.target.value)}
-              />
-              <button onClick={() => Export_Excel(spread)}>저장하기</button>
-            </div>
-            <div>
-              <h4> 특정 셀(Cell) 데이터 가져오기 </h4>
-              <button onClick={() => GetCelData(spread)}>콘솔로 A1 & B2:D4 값 가져오기</button>
-            </div>
-          </div>
+          <h4> Excel 가져오기 </h4>
+          <input type="file" id="fileDemo" accept=".xlsx" />
+          <button onClick={() => ImportFile(spread)}>불러오기</button>
+        </div>
+        <div>
+          <h4> Excel 내보내기 </h4>
+          파일명:
+          <input
+            type="text"
+            id="exportFileName"
+            value={minutes}
+            onChange={(e) => setMinutes(e.target.value)}
+          />
+          <button onClick={() => Export_Excel(spread)}>저장하기</button>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Report
+export default Report;
